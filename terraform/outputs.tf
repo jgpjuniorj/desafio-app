7b69645d92147e8e6b06ae563196dde1d94e2050
junior <junior.jair.sk8@gmail.com>
2025-01-22 14:47:42 -0300

    estruturado o repositorio

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID da VPC criada."
+  value       = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  description = "IDs das sub-redes públicas."
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  description = "IDs das sub-redes privadas."
+  value       = aws_subnet.private[*].id
+}
+
+output "instance_ip" {
+  description = "IP público da instância EC2"
+  value       = aws_instance.grafana.public_ip
+}
