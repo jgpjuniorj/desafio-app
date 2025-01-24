0057d62a72cfe5aa52a01ce517cfa09c1eab8fb4
junior <junior.jair.sk8@gmail.com>
2025-01-24 12:35:25 -0300

    iniciando a vm com docker v34

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,8 +31,3 @@ variable "ami_id" {
   description = "AMI para a instância EC2"
   default     = "ami-0c820c196a818d66a"  # Exemplo de AMI Amazon Linux 2
 }
-
-variable "pem_file_path" {
-  description = "Path to the PEM file for SSH"
-  type        = string
-}
