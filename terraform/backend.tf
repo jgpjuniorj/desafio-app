6ebe1b3ea34badb525e38d90f76114d774cd0ab8
junior <junior.jair.sk8@gmail.com>
2025-01-24 13:41:28 -0300

    iniciando a vm com docker v52

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,7 @@
 terraform {
-    backend "s3" {}
+  backend "s3" {
+    bucket         = "jgpjuniorj-sa-east-1-terraform-statefile"
+    key            = "env:/dev/terraform-aws-base"
+    region         = "sa-east-1" # Adicione a região aqui
+  }
 }
