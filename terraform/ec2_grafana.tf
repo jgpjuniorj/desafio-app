3d76fd90389e7d39f1d9d1f18232c3581feb250a
junior <junior.jair.sk8@gmail.com>
2025-01-24 09:35:01 -0300

    iniciando a vm com docker

--- terraform/ec2_grafana.tf
+++ terraform/ec2_grafana.tf
@@ -44,15 +44,10 @@ resource "aws_instance" "grafana" {
 
   user_data = <<-EOF
     #!/bin/bash
-    yum update -y
-    amazon-linux-extras install docker -y
-    service docker start
-    usermod -a -G docker ec2-user
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-    git clone https://github.com/SEU_USUARIO/SEU_REPOSITORIO.git /home/ec2-user/grafana-setup
-    cd /home/ec2-user/grafana-setup
-    docker-compose up -d
-  EOF
+    sudo yum update -y
+    sudo yum install -y docker
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    EOF
 
 }
