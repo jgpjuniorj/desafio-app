753320ba3b4e0bb0a69dc40b3ba0ccadbed9b569
junior <junior.jair.sk8@gmail.com>
2025-01-30 16:19:58 -0300

    construindo v4

--- app/start.sh
+++ app/start.sh
@@ -3,7 +3,7 @@ echo "Iniciando app na porta 5000..."
 ./venv/bin/gunicorn --bind 0.0.0.0:5000 --timeout 240 app:app &
 
 echo "Aguardando 5 segundos..."
-sleep 5
+sleep 10
 
 echo "Iniciando redirect na porta 5001..."
 ./venv/bin/gunicorn --bind 0.0.0.0:5001 --timeout 240 redirect:app &
