68e1383a7e6e1ba6d289d943e79d5d9bed503a8c
junior <junior.jair.sk8@gmail.com>
2025-01-30 16:03:02 -0300

    construindo v2

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# start.sh
-./venv/bin/gunicorn --bind 0.0.0.0:5000 --timeout 120 app:app &
-./venv/bin/gunicorn --bind 0.0.0.0:5001 --timeout 120 redirect:app &
+echo "Iniciando app na porta 5000..."
+./venv/bin/gunicorn --bind 0.0.0.0:5000 --timeout 240 app:app &
+
+echo "Aguardando 5 segundos..."
+sleep 5
+
+echo "Iniciando redirect na porta 5001..."
+./venv/bin/gunicorn --bind 0.0.0.0:5001 --timeout 240 redirect:app &
+
+echo "Aguardando processos em segundo plano..."
 wait
