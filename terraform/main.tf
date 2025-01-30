57002dce62966a147c2f8662a7ac19afb1b44619
junior <junior.jair.sk8@gmail.com>
2025-01-29 22:03:20 -0300

    teste - 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-# Recursos de Rede
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -44,6 +43,7 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+# Removido o Elastic IP e o NAT Gateway, pois não são necessários
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   tags = {
@@ -51,10 +51,11 @@ resource "aws_route_table" "private" {
   }
 }
 
+# Ajuste na rota para sub-redes privadas sem NAT Gateway
 resource "aws_route" "private_internet_access" {
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main.id
+  gateway_id             = aws_internet_gateway.main.id # Usando o Internet Gateway para tráfego de saída
 }
 
 resource "aws_subnet" "private" {
@@ -70,66 +71,4 @@ resource "aws_route_table_association" "private" {
   count          = length(var.private_subnet_cidrs)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
-}
-
-# Security Group para o ALB e ECS
-resource "aws_security_group" "app" {
-  name        = "app-sg"
-  description = "Security group para a aplicação"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Instância EC2 para o Grafana
-resource "aws_instance" "grafana" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = "grafana_key_git"
-  subnet_id     = aws_subnet.public[0].id
-  vpc_security_group_ids = [aws_security_group.grafana_sg.id]
-
-  tags = {
-    Name = "Grafana EC2 Instance"
-  }
-}
-
-# Security Group para o Grafana
-resource "aws_security_group" "grafana_sg" {
-  name        = "grafana-sg"
-  description = "Security group para o Grafana"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -22,8 +22,3 @@ output "alb_dns_name" {
   description = "DNS do ALB"
   value       = aws_lb.main.dns_name
 }
-
-output "ecr_repository_url" {
-  description = "URL do repositório ECR"
-  value       = aws_ecr_repository.app.repository_url
-}
