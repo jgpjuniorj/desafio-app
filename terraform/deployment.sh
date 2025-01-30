09ea82568cc169e0638b522b791ef30260f5422f
junior <junior.jair.sk8@gmail.com>
2025-01-30 02:14:40 -0300

    subindo docker 4

--- terraform/deployment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# install and configure docker on the ec2 instance
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo systemctl enable docker
