8c9d72b29114468ced4948ddd0feb78091b0f0d2
junior <junior.jair.sk8@gmail.com>
2025-01-30 16:55:20 -0300

    construindo v7

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -92,41 +92,7 @@ resource "aws_ecs_service" "app_service" {
     security_groups  = [aws_security_group.app_sg.id]
     assign_public_ip = true
   }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.app_tg.arn
-    container_name   = "app"
-    container_port   = 5000
-  }
 }
-
-resource "aws_lb" "app_lb" {
-  name               = "app-load-balancer"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.app_sg.id]
-  subnets            = aws_subnet.public[*].id
-}
-
-resource "aws_lb_target_group" "app_tg" {
-  name     = "app-target-group"
-  port     = 5000
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
-
-resource "aws_lb_listener" "app_listener" {
-  load_balancer_arn = aws_lb.app_lb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
-
-
 
 resource "aws_iam_policy" "ecs_ecr_policy" {
   name        = "ecs_ecr_policy"
